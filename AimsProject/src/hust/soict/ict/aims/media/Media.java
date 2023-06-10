8a6fc8d7e5f3a17d89a7fcc727c1debe0ad87ab2
Fauna <duong.tn210263@sis.hust.edu.vn>
2023-06-10 11:03:40 +0700

    Adding Media abstract class as base for DigitalVideoDisc and book

---
+++ AimsProject/src/hust/soict/ict/aims/media/Media.java
@@ -0,0 +1,40 @@
+package hust.soict.ict.aims.media;
+
+public abstract class Media {
+    protected int id;
+    protected String title;
+    protected String category;
+    protected float cost;
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+}
